8113a4cff35abbe9c0b793019a8a30e0665de958
Sean Hardy <seanjhardy@icloud.com>
2024-04-29 12:05:00 +0100

    AFTER 1H: Added comments, improvements and extensions

--- src/components/currencyList/currencyList.js
+++ src/components/currencyList/currencyList.js
@@ -7,6 +7,7 @@ export const CurrencyList = () => {
   const [currencyData, setCurrencyData] = useState({})
 
   useEffect(() => {
+    //TODO: Move this function outside of useEffect for readability
     const fetchData = async () => {
       const currencyData = await getCurrencies()
       setCurrencyData(currencyData)
@@ -16,9 +17,11 @@ export const CurrencyList = () => {
 
   return (
     /**
-     * Make this height dynamic instead of fixed, couldn't get it to fit in the bounds
+     * TODO: Make this height dynamic instead of fixed, (couldn't get it to fit in the bounds).
+     * Removed horizontal scrollbar and improve existing scrollbar UI using dark theme
      */
     <div className={"container"} style={{height: 400}}>
+      {/*TODO: Add a search bar here to filter the resulting currencies*/}
       <div className={"list"}>
         {Object.entries(currencyData).map(([currency, values]) => {
           return <CurrencyItem currency={currency} data={values}/>
