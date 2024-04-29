01b893d1756e0b6c5741640ad3874e3ec61d77c0
Sean Hardy <seanjhardy@icloud.com>
2024-04-29 11:11:52 +0100

    Added convertCurrency API method

--- src/services/api.js
+++ src/services/api.js
@@ -7,3 +7,13 @@ export const getCurrencies = () => {
       console.error('Error fetching data:', error);
     });
 }
+
+export const convertCurrency = ({amountA, typeA, typeB="btc"}) => {
+  return fetch(`https://blockchain.info/tobtc?currency=${typeA}&value=${amountA}`)
+    .then(response => response.json())
+    .then((data) => {
+      return data
+    }).catch(error => {
+      console.error('Error fetching data:', error);
+    });
+}
